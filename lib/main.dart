9b53642fbb6fa76fb3c5eb7f9f89dc1cb0b1ad98
chutima <chutimamaa2544@gmail.com>
2022-07-20 23:40:01 +0700

    setstate

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,11 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            number++;
+          });
+        },
         child: Icon(Icons.add),
       ),
     );
