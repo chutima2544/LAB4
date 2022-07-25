eb06e5196a9ac42e7846c116c81dfd8cd4c0cc0c
chutima <chutimamaa2544@gmail.com>
2022-07-25 16:13:24 +0700

    internal

--- lib/main.dart
+++ lib/main.dart
@@ -25,23 +25,24 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   //กลุ่มข้อมูล
   List<FoodMenu> menu=[
-    FoodMenu("ข้าวผัด", "50"),
-    FoodMenu("ผัดไทย", "50"),
-    FoodMenu("ข้าวต้ม", "50"),
-    FoodMenu("ข้าวหน้าหมูทอด", "50"),
+    FoodMenu("ข้าวผัด", "50","assets/images/pic1.jpg"),
+    FoodMenu("ผัดไทย", "50","assets/images/pic2.jpg"),
+    FoodMenu("โกโก้", "50","assets/images/pic3.jpg"),
+    FoodMenu("ชานมไข่มุก", "50","assets/images/pic4.jpg")
   ];
   //แสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("รายการอาหาร"),
+          title: Text("รายการอาหารและเครื่องดื่ม"),
         ),
         body: ListView.builder(
           itemCount: menu.length,
           itemBuilder: (BuildContext context, int index) {
             FoodMenu food=menu[index];
           return ListTile(
+            leading: Image.asset(food.img),
             title: Text(food.name,style: TextStyle(fontSize: 25),),
             subtitle: Text("ราคา "+food.price+" บาท"),
           );
